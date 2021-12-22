ffb6d5a8ed0d116c2be3579491b7e159cac5b136
aramkoukia <aramkoukia@gmail.com>
2021-12-22 11:01:30 -0800

    rendering customer orders

--- assets/customer-orders.js
+++ assets/customer-orders.js
@@ -2,8 +2,16 @@ async function getOrders() {
   alert("starting to get orders");
   let x = await fetch("https://lightsandpartsapi.azurewebsites.net/api/shopify-storefront/customer-orders?customerid=29847");
   let y = await x.text();
-  alert(y);
-}
 
+  alert(y.length);
+
+  for (i = 0; i < y.length; i++) {
+    recommendedProductsHTML += `<div>${recommendedProducts[i].title}</div>`;
+  }
+
+  $("#customer_orders").html(recommendedProductsHTML);
+
+  // alert(y);
+}
 
 getOrders();
