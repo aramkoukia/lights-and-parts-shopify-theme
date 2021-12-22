d29ea57dd414ca4e8baba49f4a407eb8e983c997
aramkoukia <aramkoukia@gmail.com>
2021-12-22 11:05:06 -0800

    fixing order fields

--- assets/customer-orders.js
+++ assets/customer-orders.js
@@ -1,15 +1,15 @@
 async function getOrders() {
   alert("starting to get orders");
-  let x = await fetch("https://lightsandpartsapi.azurewebsites.net/api/shopify-storefront/customer-orders?customerid=29847");
+  let x = await fetch("https://lightsandpartsapi.azurewebsites.net/api/shopify-storefront/customer-orders?customerid=10532");
   let y = await x.text();
 
   alert(y.length);
 
   for (i = 0; i < y.length; i++) {
-    recommendedProductsHTML += `<div>${recommendedProducts[i].title}</div>`;
+    ordrsHTML += `<div>${y[i].OrderId}-${y[i].OrderDate}-${y[i].Total}</div>`;
   }
 
-  $("#customer_orders").html(recommendedProductsHTML);
+  $("#customer_orders").html(ordrsHTML);
 
   // alert(y);
 }
