5c9b8af0a6c3a609b0a45606a8c91ca0bda0739e
aramkoukia <aramkoukia@gmail.com>
2021-12-22 11:10:36 -0800

    jesus fucking christ!

--- assets/customer-orders.js
+++ assets/customer-orders.js
@@ -2,7 +2,7 @@ async function getOrders() {
   fetch("https://lightsandpartsapi.azurewebsites.net/api/shopify-storefront/customer-orders?customerid=10532")
     .then(response => response.json())
     .then(orders => {
-      const ordrsHTML = "";
+      let ordrsHTML = "";
       orders.forEach(order => {
         ordrsHTML += `<div>${order.orderId}-${order.orderDate}-${order.total}</div>`;
       });
