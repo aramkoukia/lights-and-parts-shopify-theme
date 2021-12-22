13ce28d03c015371a71d99b0260df361a959f049
aramkoukia <aramkoukia@gmail.com>
2021-12-22 11:07:19 -0800

    fixing javascript for orders

--- assets/customer-orders.js
+++ assets/customer-orders.js
@@ -1,17 +1,13 @@
 async function getOrders() {
-  alert("starting to get orders");
-  let x = await fetch("https://lightsandpartsapi.azurewebsites.net/api/shopify-storefront/customer-orders?customerid=10532");
-  let y = await x.text();
+  fetch("https://lightsandpartsapi.azurewebsites.net/api/shopify-storefront/customer-orders?customerid=10532")
+    .then(response => response.json())
+    .then(data => {
+      for (i = 0; i < y.length; i++) {
+        ordrsHTML += `<div>${y[i].OrderId}-${y[i].OrderDate}-${y[i].Total}</div>`;
+      }
 
-  alert(y.length);
-
-  for (i = 0; i < y.length; i++) {
-    ordrsHTML += `<div>${y[i].OrderId}-${y[i].OrderDate}-${y[i].Total}</div>`;
-  }
-
-  $("#customer_orders").html(ordrsHTML);
-
-  // alert(y);
+      $("#customer_orders").html(ordrsHTML);
+    });
 }
 
 getOrders();
