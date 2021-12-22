c12b0e2d4723e66e286762cd9f2b2293774bba27
aramkoukia <aramkoukia@gmail.com>
2021-12-22 11:08:35 -0800

    still not working

--- assets/customer-orders.js
+++ assets/customer-orders.js
@@ -1,11 +1,10 @@
 async function getOrders() {
   fetch("https://lightsandpartsapi.azurewebsites.net/api/shopify-storefront/customer-orders?customerid=10532")
     .then(response => response.json())
-    .then(data => {
-      for (i = 0; i < y.length; i++) {
-        ordrsHTML += `<div>${y[i].OrderId}-${y[i].OrderDate}-${y[i].Total}</div>`;
-      }
-
+    .then(orders => {
+      orders.forEach(order => {
+        ordrsHTML += `<div>${order.OrderId}-${order.OrderDate}-${order.Total}</div>`;
+      });
       $("#customer_orders").html(ordrsHTML);
     });
 }
